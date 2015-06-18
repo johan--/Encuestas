a139ee2fcb38ea0df7f0bd64818bff76c6d9c995
Alejandro Masciotra <kaloye_ale@hotmail.com>
2015-06-18 08:04:02 -0300

    obtengo path de imagenes

--- RubraEncuesta7/clases/mostrarEncuestaValida.php
+++ RubraEncuesta7/clases/mostrarEncuestaValida.php
@@ -2,8 +2,7 @@
 include("db.php");
 session_start();
 
-
-$select="SELECT fechaFin, estado FROM  `encuestas` WHERE id = '".$_GET["ehIdi"]."'; ";
+$select=" SELECT en.fechaFin, en.estado , eve.`imagen_ruta` imagen_ruta_eve , eve.`imagen_nombre` imagen_nombre_eve, cli.`imagen_ruta` imagen_ruta_cli , cli.`imagen_nombre` imagen_nombre_cli FROM  `encuestas` en  left join `eventos` eve on (eve.id = en.idEvento) left join `clientes` cli on (eve.`idCliente` = cli.id) WHERE en.id = '".$_GET["ehIdi"]."'; ";
 
 	//CONSULTA
 	$result=mysqli_query($db,$select);
@@ -12,6 +11,10 @@ $select="SELECT fechaFin, estado FROM  `encuestas` WHERE id = '".$_GET["ehIdi"].
 	while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){ 
 			$lista['fechaFin']=$row ["fechaFin"];
 			$lista['estado']=$row ["estado"];
+			$lista['imagen_ruta_eve']=$row ["imagen_ruta_eve"];
+			$lista['imagen_nombre_eve']=$row ["imagen_nombre_eve"];
+			$lista['imagen_ruta_cli']=$row ["imagen_ruta_cli"];
+			$lista['imagen_nombre_cli']=$row ["imagen_nombre_cli"];
 	}
 
 
